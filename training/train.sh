f267020a780468da0792dc09ebc465c87b8d0fe6
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-08-29 23:19:39 +0700

    u

--- training/train.sh
+++ training/train.sh
@@ -8,8 +8,8 @@ ARGS="
 --run_name flamingo-tiny-vitL
 --do_train --do_eval
 --optim adamw_torch
---num_train_epochs 20
---learning_rate 20.0 
+--num_train_epochs 50
+--learning_rate 3.0 
 --warmup_steps 5000
 --lr_scheduler_type constant_with_warmup
 --per_device_train_batch_size 8
