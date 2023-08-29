6bc0b744fb4da6df74242ed119e957fe8c193333
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-08-29 14:46:35 +0700

    u

--- training/train.sh
+++ training/train.sh
@@ -6,17 +6,17 @@ NUM_GPU=2
 ARGS="
 --output_dir ./flamingo-coco
 --run_name flamingo-tiny-vitL
---do_train
+--do_train --do_eval
+--num_train_epochs 10
 --optim adamw_torch
 --learning_rate 0.0001 
 --warmup_steps 5000
 --lr_scheduler_type constant_with_warmup
---per_device_train_batch_size 4
---per_device_eval_batch_size 4
+--per_device_train_batch_size 8
+--per_device_eval_batch_size 64
 --gradient_accumulation_steps 1
 --evaluation_strategy steps
 --eval_steps 1
---num_train_epochs 1
 --save_strategy epoch
 --save_total_limit 2
 --log_level info
@@ -25,13 +25,11 @@ ARGS="
 --report_to wandb
 --ddp_find_unused_parameters False
 "
-
 echo $ARGS
-
 if [ $NUM_GPU == 1 ]; then
     echo "running on a single GPU"
-    python ./train.py $ARGS
+    python ./training/train.py $ARGS
 else
     echo "running on multiple GPUs"
-    torchrun --nproc_per_node $NUM_GPU ./train.py $ARGS
+    torchrun --nproc_per_node $NUM_GPU ./training/train.py $ARGS
 fi
